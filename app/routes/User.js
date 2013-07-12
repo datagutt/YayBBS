07be89996f47a632a5352eee53d64a4e0417d597
datagutt <datagutt@lekanger.no>
2013-07-12 18:23:55 +0200

    Last.fm on profiles

--- app/routes/User.js
+++ app/routes/User.js
@@ -1,7 +1,45 @@
 require('date-format-lite');
+var request = require('request');
 module.exports = function(app, models){
 	var Thread = models.Thread;
 	var User = models.User;
+	
+	var lastfm = function(username, cb){
+		var songs = [], i = 0;
+		request('http://ws.audioscrobbler.com/1.0/user/' + username + '/recenttracks.txt', function(error, response, body){
+			if(error){
+				cb(error);
+			}
+			if(body){
+				body = body.split('\n');
+				if(!body){
+					cb();
+					return;
+				}
+				body.forEach(function(data){
+					if(data){
+						var parts = data.split(',');
+						if(parts && parts[0] && parts[1]){
+							var songinfo = parts[1].split(' – ');
+							var artist = songinfo[0],
+								name = songinfo[1];
+							songs.push({
+								'artist': artist,
+								'name': name,
+								'time': parts[0]
+							});
+						}
+					}
+					if(i == body.length - 1){						
+						if(typeof cb == 'function'){
+							cb(false, songs);
+						}
+					}
+					i++;
+				});
+			}
+		});
+	};
 	
 	app.get('/user/:username', function(req, res){
 		User.findOne({username: req.params.username}, function(err, user){
@@ -19,15 +57,18 @@ module.exports = function(app, models){
 				}
 				var since = date.format('MMMM, YYYY');
 				Thread.count({user_id: user.id}, function(err, threadCount){
-					res.render('partials/user', {
-						'user': {
-							'isYou': (req.session.user && user.username == req.session.user.username),
-							'username': user.username,
-							'since': since,
-							'threads': threadCount || 0,
-							'comments': 0,
-							'services': user.services || {}
-						}
+					lastfm(user.services.lastfm ? user.services.lastfm.username : '', function(err, data){
+						res.render('partials/user', {
+							'user': {
+								'isYou': (req.session.user && user.username == req.session.user.username),
+								'username': user.username,
+								'since': since,
+								'threads': threadCount || 0,
+								'comments': 0,
+								'services': user.services || {},
+								'lastfm': data
+							}
+						});
 					});
 				});
 			}else{
